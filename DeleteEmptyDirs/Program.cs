a09e3fc7ef96911096f4b0990b64deae9edd648d
BIBI-NITRO <BIBI-NITRO@DESKTOP-4JRHM8G>
2022-06-25 11:28:55 +0800

    Add project files.

---
+++ DeleteEmptyDirs/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        SimpleLogger log = new SimpleLogger();
+        processDirectory(@"d:\OneDrive",log);
+    }
+
+    private static void processDirectory(string startLocation, SimpleLogger log)
+    {
+        foreach (var directory in Directory.GetDirectories(startLocation))
+        {
+            processDirectory(directory,log);
+            if (Directory.GetFiles(directory).Length == 0 &&
+                Directory.GetDirectories(directory).Length == 0)
+            {
+                log.Info("Deleting empty dir " + directory);
+                Directory.Delete(directory, false);
+            }
+        }
+    }
+}
